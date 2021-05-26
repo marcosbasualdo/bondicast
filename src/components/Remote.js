354849a46b897a80f3a2f5b1931a84302058bd7e
Marcos Basualdo <marcosbasualdo@gmail.com>
2021-05-26 01:34:06 -0300

    Added icons to remote

--- src/components/Remote.js
+++ src/components/Remote.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
 
 function Remote({onPlay, onPause, paused}){
     return (
-        <div className="d-grid gap-2">
-            {paused && (<button className="btn btn-primary btn-lg" onClick={onPlay}>Play</button>)}
-            {!paused && (<button className="btn btn-secondary btn-lg" onClick={onPause}>Pause</button>)}
+        <div className="d-grid gap-2 remote">
+            {paused && (<button className="btn btn-primary btn-lg" onClick={onPlay}>
+                <PlayArrowIcon></PlayArrowIcon>
+            </button>)}
+            {!paused && (<button className="btn btn-secondary btn-lg" onClick={onPause}>
+                <PauseIcon></PauseIcon>
+            </button>)}
         </div>        
     )
 }
